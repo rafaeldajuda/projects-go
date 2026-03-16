9a4abd0b70c1920ba2b3a93fbe8fe3f17cb2c0ec
rafdeajuda <rafael.ajuda@engdb.com.br>
2026-03-16 16:38:40 -0300

    urbanstay-api - add handler

---
+++ apps/urbanstay-api/internal/delivery/http/handler.go
@@ -0,0 +1,34 @@
+package http
+
+import (
+	"urbanstay-api/internal/domain/entity"
+	"urbanstay-api/internal/usercase"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+type PropertyHandler struct {
+	uc *usercase.PropertyUseCase
+}
+
+func NewPropertyHandler(uc *usercase.PropertyUseCase) *PropertyHandler {
+	return &PropertyHandler{uc: uc}
+}
+
+func (h *PropertyHandler) CreateProperty(c fiber.Ctx) error {
+	var property entity.Property
+
+	// pegando o body do request
+	if err := c.Bind().Body(&property); err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	// chamando usercase passando o body
+	err := h.uc.ExecuteCreate(c.Context(), &property)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	// resposta de sucesso
+	return c.SendStatus(fiber.StatusCreated)
+}
